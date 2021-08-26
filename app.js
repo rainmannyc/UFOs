0f8276188301331e8f1d691ee0f63870fc8b7e23
rainmannyc <84225574+rainmannyc@users.noreply.github.com>
2021-08-26 07:42:05 -0400

    mod 11.6.1

--- app.js
+++ app.js
@@ -17,3 +17,21 @@ data.forEach((dataRow) => { //loop through each object in the data into "dataRow
     }
     );
 });
+
+function handleClick(){
+    let date = d3.select("#datetime").property("value"); // Grab the datetime value from the filter / date = using d3 to look and select datetime that we'll put in our html and property("value") command for storing into "date" variable
+    let filteredData = tableData; // setting filteredData var as the clean tableData we used originally
+    
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) { // if date is passed through, use filter() on filteredData (passing in 'row'/"with 'row' ", row.datetime strictly equals date     
+        filteredData = filteredData.filter(row => row.datetime === date); 
+    };       // "*Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value"
+    buildTable(filteredData); // Rebuild the table using the filtered data / buildtable function with filteredData passing in the parameters
+ };
+
+ // d3 select all function: listen to all from "filter-btn" , and '.on', "click", run function handleClick, passed through the parameters
+ d3.selectALL("#filter-btn").on("click", "handleClick")
+
+ // build the table a
+ buildTable(tableData);
